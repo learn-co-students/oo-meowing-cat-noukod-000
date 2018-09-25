b366aba029318d38fb6acecfe9216e2f54e90215
Samuel Cornet <samuel@noukod.com>
2018-09-25 17:36:20 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,11 @@
-## code your solution here. 
+class Cat
+ attr_accessor :name, :meow
+end
+
+nitty = Cat.new
+nitty.name = "Nitty"
+puts nitty.name
+
+meow = Cat.new
+nitty.meow = "Meow"
+puts nitty.meow
